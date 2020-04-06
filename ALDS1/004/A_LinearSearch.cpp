ac009aaa8af6de0eea822549fa85637de0821817
Nattchi <Yuu.tan@icloud.com>
2020-04-07 01:11:42 +0900

    A_LinearSearch 追加

---
+++ ALDS1/004/A_LinearSearch.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n, q;
+    cin >> n;
+    vector<long long> S(n);
+    for (int i = 0; i < n; i++){
+        cin >> S[i];
+    }
+    cin >> q;
+    vector<long long> T(q);
+    for (int i = 0; i < q; i++){
+        cin >> T[i];
+    }
+    int matched = 0;
+    for (int i = 0; i < q; i++){
+        for (int j = 0; j < n; j++){
+            if(S[j] == T[i]){
+                matched++;
+                break;
+            }
+        }
+    }
+    cout << matched << endl;
+}
