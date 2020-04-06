b0f8eb5cbf3a9e4ce3a2ad7cf30107d908143eda
Nattchi <Yuu.tan@icloud.com>
2020-04-07 01:28:30 +0900

    A_LinearSearch_Sentinel 追加

---
+++ ALDS1/004/A_LinearSearch_Sentinel.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+bool linearSearch(vector<long long> A, int n, long long key){
+    int i = 0;
+    A[n] = key;//番兵
+    while(A[i] != key){
+        i++;
+    }
+    return i != n;
+}
+
+int main(){
+    int n, q;
+    cin >> n;
+    vector<long long> S(n + 1);
+    for (int i = 0; i < n; i++){
+        cin >> S[i];
+    }
+    cin >> q;
+    int matched = 0;
+    vector<long long> T(q);
+    for (int i = 0; i < q; i++){
+        cin >> T[i];
+        if (linearSearch(S, n, T[i])) matched++;
+    }
+    cout << matched << endl;
+}
