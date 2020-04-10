4dd41464a00be007a163d9967ee2e8a2a61564b2
Nattchi <Yuu.tan@icloud.com>
2020-04-11 01:16:40 +0900

    A_Stack 追加

---
+++ ALDS1/003/A_Stack.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Stack{
+private:
+    vector<string> s;
+public:
+    Stack(vector<string> str){
+        s = str;
+    }
+
+    void push(string x){
+        s.push_back(x);
+    }
+
+    string pop(){
+        string back = s.back();
+        s.pop_back();
+        return back;
+    }
+
+    bool isEmpty(){
+        return s.empty();
+    }
+};
+
+int main(){
+    vector<string> str(200);
+    Stack stack = Stack(str);
+    string s;
+    long long a, b;
+
+    while(cin >> s){//入力の終わりまで処理
+        if(s == "+"){
+            b = stoll(stack.pop());
+            a = stoll(stack.pop());
+            stack.push(to_string(a + b));
+        }else if(s == "-"){
+            b = stoll(stack.pop());
+            a = stoll(stack.pop());
+            stack.push(to_string(a - b));
+        }else if(s == "*"){
+            b = stoll(stack.pop());
+            a = stoll(stack.pop());
+            stack.push(to_string(a * b));
+        }else{
+            stack.push(s);
+        }
+    }
+    cout << stack.pop() << endl; 
+}
