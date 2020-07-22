89258503185eaf75c2e864a395bd377595b07a7a
Nattchi <Yuu.tan@icloud.com>
2020-07-22 21:07:31 +0900

    A_Graph 追加

---
+++ ALDS1/011/A_Graph.cpp
@@ -0,0 +1,27 @@
+﻿﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, u, k, e;
+    cin >> n;
+
+    vector<vector<int> > V(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        cin >> u >> k;
+        for (int j = 0; j < k; j++) {
+            cin >> e;
+            V[u - 1][e - 1] = u;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j == n-1) {
+                cout << V[i][j] << endl;
+                continue;
+            }
+            cout << V[i][j] << " ";
+        }
+    }
+}
